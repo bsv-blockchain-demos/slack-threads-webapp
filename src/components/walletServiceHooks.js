74bb4073fbeb142507ea94ce25e401f533ec6b7a
Kjartan <kjartan@oskarsson.nl>
2025-06-24 01:49:10 +0200

    Metanet walletclient initial setup

---
+++ src/components/walletServiceHooks.js
@@ -0,0 +1,67 @@
+import { Script, WalletClient } from '@bsv/sdk';
+
+export async function connectWallet() {
+    // Connect to wallet with BEEF capabilities
+    const wallet = new WalletClient('json-api', 'localhost:3000'); //TODO: Add new Metanet App
+    const isConnected = await wallet.isAuthenticated();
+
+    if (isConnected) {
+        return wallet;
+    }
+
+    return null;
+}
+
+export async function createVoteToken(type) {
+    const wallet = await connectWallet();
+
+    if (!wallet) {
+        throw new Error('Wallet not connected');
+    }
+
+    let response;
+
+    // Create atomic operation
+    response = await wallet.createAction({
+        description: `Create ${type} token`,
+        outputs: [{
+            satoshis: 1,
+            lockingScript: Script.fromASM('OP_NOP').toHex(), //TODO: scriptTemplate
+            basket: type,
+            outputDescription: `${type} token`
+        }]
+    });
+
+
+    // Wallet returns BEEF format for P2P transmission
+    // Contains all SPV data needed for instant verification
+    return response;
+}
+
+export async function redeemVote(type, txID) {
+    const wallet = await connectWallet();
+
+    if (!wallet) {
+        throw new Error('Wallet not connected');
+    }
+
+    //TODO: List all Outpoints and find the correct one using the MongoDB txID
+    const outpoints = await wallet.listOutpoints({ basket: type, include: "entire transactions" });
+    const txBEEF = outpoints.BEEF;
+    const voteOutpoint = outpoints.outputs.find(output => output.outpoint.strip('.')[0] === txID);
+
+    // Redeem vote
+    const response = await wallet.createAction({
+        description: 'Redeem vote',
+        inputBEEF: txBEEF,
+        inputs: [{
+            inputDescription: 'Redeem vote',
+            unlockingScript: Script.fromASM('OP_TRUE').toHex(), //TODO: scriptTemplate
+            outpoint: voteOutpoint.outpoint,
+        }]
+    });
+
+    // Wallet returns BEEF format for P2P transmission
+    // Contains all SPV data needed for instant verification
+    return response;
+}
